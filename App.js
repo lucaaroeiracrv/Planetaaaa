528c5fe84343695f99fa8eea0bcf0639573be8fa
lucaaroeiracrv <lucaaroeira@gmail.com>
2024-04-25 00:32:25 -0300

    feat: Correcao da atitudinal

--- App.js
+++ App.js
@@ -7,19 +7,20 @@ import * as React from 'react';
 
 
 export default function App() {
-  return(
+  return (
     <View style={styles.container}>
-      <TopArea></TopArea>
-      <View style={styles.topArea} />
-      <View style={styles.content}>
-        <Text style={styles.title}>Hello!</Text>
-        <Text style={styles.text}>
-          Want to know and explain all things about the planets in the Milky Way galaxy?
-        </Text>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Explore Now</Text>
-        </TouchableOpacity>
-      </View>
+      <TopArea/>
+        <View style={styles.topArea} />
+        <View style={styles.content}>
+          <Text style={styles.title}>Hello!</Text>
+          <Text style={styles.text}>
+            Want to know and explain all things about the planets in the Milky Way galaxy?
+          </Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Explore Now</Text>
+          </TouchableOpacity>
+        </View>
+        <TopArea />
     </View>
   );
 };
--- src/constants/style.js
+++ src/constants/style.js
@@ -1,45 +1,51 @@
+// Arquivo 1: styles.js
+
 import { StyleSheet } from "react-native"
 import { height, width } from "./dimensions"
 
 export default StyleSheet.create({
-
-    backgroundImage: {
-        width: width,
-        height: height,
-    },
-    container: {
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        backgroundColor:"white"
-    },
-    content: {
-        width: width,
-        backgroundColor: 'transparent',
-        padding: 50,
-        margin: 15,
-        borderRadius: 28,
-        alignItems: 'center',
-    },
-      title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-      },
-      text: {
-        fontSize: 18,
-        marginBottom: 20,
-        textAlign: 'center',
-      },
-      button: {
-        backgroundColor: '#e94668',
-        padding:20,
-        borderRadius: 5,
-        width:width*0.7,
-        alignItems: 'center',
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 16,
-        fontSize: 20,
-      },
-    });
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: width,
+    height: height,
+    zIndex: -1, 
+  },
+  container: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: "white",
+    flex: 1,
+    paddingBottom: 20,
+    paddingHorizontal: 20,
+  },
+  content: {
+    width: width - 20, 
+    padding: 50,
+    borderRadius: 28,
+    alignItems: 'center',
+    backgroundColor: "white"
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#e94668',
+    padding: 20,
+    borderRadius: 32,
+    width: width * 0.7,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
+  },
+});
